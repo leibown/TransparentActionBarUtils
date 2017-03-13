2afa8137f6e440d520207f72d5eae6db5a355304
Leibown <leibown.leila@gmail.com>
2017-03-13 16:57:57 +0800

    update

--- app/src/main/java/com/leibown/transparentactionbarutils/MainActivity.java
+++ app/src/main/java/com/leibown/transparentactionbarutils/MainActivity.java
@@ -12,10 +12,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View actionBar = View.inflate(this, R.layout.layout_actionbar, null);
-        View contentView = TransparentActionBarUtils.getTransparentActionBar(this, R.layout.activity_main, actionBar);
+        View contentView = TransparentActionBarUtils.getTransparentActionBar(this, R.layout.activity_main);
         setContentView(contentView);
 
 //        TransparentActionBarUtils.setActionBarBackgroudColor(this, Color.parseColor("#FF0055"));
         TransparentActionBarUtils.setActionBarBackgroudDrawable(this, R.drawable.bg_nav);
+    }
+
+    private boolean isOk = false;
+
+    public void doClick(View v) {
+        if (isOk) {
+            TransparentActionBarUtils.hideTransparentActionBar(this);
+        } else {
+            TransparentActionBarUtils.showTransparentActionBar(this);
+        }
+        isOk = !isOk;
     }
 }
--- library/src/main/java/com/leibown/library/TransparentActionBarUtils.java
+++ library/src/main/java/com/leibown/library/TransparentActionBarUtils.java
@@ -15,10 +15,20 @@ import android.widget.LinearLayout;
 
 public class TransparentActionBarUtils {
 
+    public static View getTransparentActionBar(Activity activity, int resId) {
+        return getTransparentActionBar(activity, resId, null);
+    }
+
     public static View getTransparentActionBar(Activity activity, int resId, View actionBar) {
         LayoutInflater inflater = activity.getLayoutInflater();
         LinearLayout containerView = (LinearLayout) inflater.inflate(R.layout.activity_base, null);
-        containerView.addView(inflater.inflate(resId, null));
+
+
+        View contentView = inflater.inflate(resId, null);
+        LinearLayout.LayoutParams childParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1);
+        contentView.setLayoutParams(childParams);
+        containerView.addView(contentView);
+
         LinearLayout mLlTittleBar = (LinearLayout) containerView.findViewById(R.id.ll_tittle_bar);
 
         //用来填充Android版本在4.4以上的状态栏
@@ -36,16 +46,22 @@ public class TransparentActionBarUtils {
             }
             //如果Android版本大于4.4，说明状态栏就可以被透明，我们自己的布局就可以放到状态栏之下
             //我们把自定义的ActionBar的高度增高
+            int actionBarHeight;
+            if (actionBar != null)
+                actionBarHeight = DisplayUtil.getBarHeight(activity) + DisplayUtil.dip2px(activity, 58);
+            else {
+                actionBarHeight = DisplayUtil.getBarHeight(activity);
+            }
             ViewGroup.LayoutParams params = mLlTittleBar.getLayoutParams();
-            params.height = DisplayUtil.getBarHeight(activity) + DisplayUtil.dip2px(activity, 58);
+            params.height = actionBarHeight;
             mLlTittleBar.requestLayout();
             //把用于填充状态栏的View高度设置成跟状态栏一样的高度
             ViewGroup.LayoutParams params2 = statusBar.getLayoutParams();
             params2.height = DisplayUtil.getBarHeight(activity);
             statusBar.requestLayout();
         }
-
-        mLlTittleBar.addView(actionBar);
+        if (actionBar != null)
+            mLlTittleBar.addView(actionBar);
         mLlTittleBar.setVisibility(View.VISIBLE);
         return containerView;
     }
@@ -63,4 +79,15 @@ public class TransparentActionBarUtils {
             actionBar.setBackgroundResource(drawableId);
     }
 
+    public static void hideTransparentActionBar(Activity activity) {
+        View actionBar = activity.findViewById(R.id.ll_tittle_bar);
+        if (actionBar != null)
+            actionBar.setVisibility(View.GONE);
+    }
+
+    public static void showTransparentActionBar(Activity activity) {
+        View actionBar = activity.findViewById(R.id.ll_tittle_bar);
+        if (actionBar != null)
+            actionBar.setVisibility(View.VISIBLE);
+    }
 }
